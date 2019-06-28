71d5b30a837f1e2328218e3a55a43c6e71c4b55f
Robert O'Gara <rgo594@gmail.com>
2019-06-28 15:51:51 -0400

    select which quiz functionality

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,7 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create :username => "Rob", :password_digest => "123", :exam => "SAT", :test_date => "09-11-2019", :id => 1
+
 Quiz.create :name => "SAT prep", :subject => "SAT", :id => 1
+Quiz.create :name => "Series 7 prep", :subject => "Series 7", :id => 2
+
 Question.create :description => "Choose a synonym of pretentious", :a => "austere", :b => "elated", :c => "ostentatious", :d => "ecstatic", :answer => "c", :quiz_id => 1
 Question.create :description => "Choose a synonym of boring", :a => "spartan", :b => "jejune", :c => "tawdry", :d => "muted", :answer => "b", :quiz_id => 1
 Question.create :description => "What is Gokus saiyan name?", :a => "carrot cake", :b => "caccao", :c => "crate", :d => "kakarot", :answer => "d", :quiz_id => 1
+
+Question.create :description => "Gandalfs real name is", :a => "Olórin", :b => "Mithrandir", :c => "Father Christmas", :d => "Willie Robertson", :answer => "a", :quiz_id => 2
+Question.create :description => "Jeff is allergic to?", :a => "shell fish", :b => "swimmy fish", :c => "cepholopods", :d => "sadness", :answer => "b", :quiz_id => 2
--- front-end/src/Components/QuestionList.js
+++ front-end/src/Components/QuestionList.js
@@ -9,7 +9,8 @@ class QuestionList extends Component {
     end: 1,
     counter: 100000,
     score: 0,
-    newQ: []
+    filteredQuestions: [],
+    display: false
   }
 
   componentDidMount() {
@@ -32,7 +33,9 @@ class QuestionList extends Component {
   }
 
   filterQuestions = (input_id) => {
-    this.setState({ newQ: this.state.questions.filter(question => {
+    this.setState({
+      display: true,
+      filteredQuestions: this.state.questions.filter(question => {
        return question.quiz_id === input_id
     })})
   }
@@ -41,17 +44,24 @@ class QuestionList extends Component {
     // const interval = setInterval(() => {
     //   this.setState({counter: --this.state.counter})
     //   }, 1000)
-    console.log(this.state.newQ)
+    console.log(this.state.filteredQuestions)
 
-    const question = this.state.questions.slice(this.state.start, this.state.end).map(q => {
+    const question = this.state.filteredQuestions.slice(this.state.start, this.state.end).map(q => {
       return <Question setScore={this.setScore} question={q} nextQuest={this.nextQuest} />
     })
     return (
 
       <div>
-        <button onClick={() => {this.filterQuestions(1)}}>work please</button>
-        <h1>Score: {this.state.score} </h1>
-        {question}
+        <button onClick={() => {this.filterQuestions(1)} }>SAT</button>
+        <button onClick={() => {this.filterQuestions(2)} }>Series 7</button>
+
+        {this.state.display ?
+        <div>
+          <h1>Score: {this.state.score} </h1>
+          {question}
+        </div>
+        : <h4>Choose a category</h4>
+        }
       </div>
     );
   }
